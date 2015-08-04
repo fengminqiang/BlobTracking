ff7544c5877a1b2f50433eca5b7ca6dd552b1b1e
豐敏強 <879376534@qq.com>
2015-08-04 09:37:36 +0800

    Initial Commit

---
+++ OpenCVTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCVTest
+//
+//  Created by Feng on 15/8/4.
+//  Copyright © 2015年 Feng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
